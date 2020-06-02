76f68ec9c456d80ca726206ba8a1e0a9ecd32bac
Rahul Ahuja <rahul.ahuja@gocomet.in>
2020-06-03 01:41:24 +0530

    init

---
+++ lib/illegal_class_cop.rb
@@ -0,0 +1,6 @@
+require "illegal_class_cop/version"
+
+module IllegalClassCop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/illegal_class_cop/version.rb
@@ -0,0 +1,3 @@
+module IllegalClassCop
+  VERSION = "0.1.0"
+end
